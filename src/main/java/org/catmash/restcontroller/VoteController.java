39ebcb4dc51cad0d68a0b11973be2a7bcb8ff358
Sebastien MANICON <smanicon@free.fr>
2019-03-17 10:34:47 +0100

    Mise en place de la partie IHM
    Permet à la partie IHM de voter

--- src/main/java/org/catmash/restcontroller/VoteController.java
+++ src/main/java/org/catmash/restcontroller/VoteController.java
@@ -4,10 +4,7 @@ import org.catmash.domain.vote.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,12 @@ public class VoteController {
         VoteGenerator voteGenerator = new VoteGenerator(catUrls, randomize);
 
         return voteGenerator.generate();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postVote() {
+
     }
 
     @ExceptionHandler(NotGeneratedVoteException.class)
